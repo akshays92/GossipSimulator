96fafb23feadddfb11c74d0eaca270f2edc8293b
Akshay Sharma <akshay.sharma@ufl.edu>
2017-09-30 17:39:57 -0400

    Initial commit -  happy durga pooja

---
+++ lib/project2.ex
@@ -0,0 +1,23 @@
+defmodule Project2 do
+  @moduledoc """
+  Documentation for Project2.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Project2.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def main(args) do
+  IO.puts ("TEST")
+  end
+
+end
