d62620ca2d28b33ca4d496ed76c64285d7e03c2e
Akshay Sharma <akshay.sharma@ufl.edu>
2017-09-30 19:10:02 -0400

    accessing command line arguements

--- lib/project2.ex
+++ lib/project2.ex
@@ -1,23 +1,30 @@
 defmodule Project2 do
   @moduledoc """
-  Documentation for Project2.
+  This is the main module of the project.
+  This module invoked by running ./project2
+
+  Below is the defination of the main function which is invoked when the application is invoked for the first time.
+  It takes input the commandline arguements and invokes the appropriate 
+  genserver modules and the n number of actors running on that genserver
+  @args numNodes topology algorithm
+  numNodes: Nnmber of nodes the topology must contain
+  topology: FullNetwork, 2DGrid, Line, Imperfect2DGrid
+  algorithm : gossip, push-sum
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Project2.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
   def main(args) do
-  IO.puts ("TEST")
+    
+    numNodes=String.to_integer(Enum.at(args,0))
+    #IO.puts is_integer(numNodes)
+    
+    topology=(Enum.at(args,1))
+    #IO.puts is_binary(topology)
+
+    algorithm=(Enum.at(args,2))
+    #IO.puts is_binary(algorithm)
+
+    IO.puts("Please run")
+    
+  
   end
 
 end
