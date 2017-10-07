5a3a2e1baf0901cfd2b6b83fa06ffe038434c34e
Akshay Sharma <akshay.sharma@ufl.edu>
2017-10-07 00:23:35 -0400

    added bonus part

--- lib/project2.ex
+++ lib/project2.ex
@@ -17,37 +17,45 @@ defmodule Project2 do
     numNodes=String.to_integer(Enum.at(args,0))
     topology=(String.upcase(Enum.at(args,1)))
     algorithm=(String.upcase(Enum.at(args,2)))
+    failureCount=(String.to_integer(Enum.at(args,3)))
     nodeList=[]
     firstNodeNo=1
     maxCount=10
-    IO.puts to_string(:os.system_time(:millisecond))<>("\tNode: "<>firstNodeNo <> "\thas has started spreading the gossip") 
     case topology do
-      "LINE" -> line(numNodes, algorithm, nil,firstNodeNo, nodeList, maxCount)
-      "2DGRID" -> imperfect2DGrid(topology,:math.ceil(:math.sqrt(numNodes))*:math.ceil(:math.sqrt(numNodes)), algorithm,1,maxCount,0,0,%{},nodeList)
-      "IMPERFECT2DGRID" -> imperfect2DGrid(topology,:math.ceil(:math.sqrt(numNodes))*:math.ceil(:math.sqrt(numNodes)), algorithm,1,maxCount,0,0,%{},nodeList)
-      "FULLNETWORK" -> fullNetwork(numNodes, algorithm,nodeList,firstNodeNo,maxCount)
+      "LINE" -> line(numNodes, algorithm, nil,firstNodeNo, nodeList, maxCount,failureCount)
+      "2DGRID" -> imperfect2DGrid(topology,:math.ceil(:math.sqrt(numNodes))*:math.ceil(:math.sqrt(numNodes)), algorithm,1,maxCount,0,0,%{},nodeList,failureCount)
+      "IMPERFECT2DGRID" -> imperfect2DGrid(topology,:math.ceil(:math.sqrt(numNodes))*:math.ceil(:math.sqrt(numNodes)), algorithm,1,maxCount,0,0,%{},nodeList,failureCount)
+      "FULLNETWORK" -> fullNetwork(numNodes, algorithm,nodeList,firstNodeNo,maxCount,failureCount)
     end
-
+    
   end
 
   #LINE ###############################################################################
   #to be called when Line topology is requested
-  def line(numNodes, algorithm, left, nodeNo, list,maxCount) when nodeNo<=numNodes do
+  def line(numNodes, algorithm, left, nodeNo, list,maxCount,failureCount) when nodeNo<=numNodes do
     {:ok, current}=Project2.LineServer.start_link(nodeNo,maxCount)
     #list=[current]++list
     Project2.LineServer.setLeft(current,left)
     Project2.LineServer.setRight(left,current)
     Project2.LineServer.setCurrent(current)
-    line(numNodes, algorithm,current,nodeNo+1, [current|list],maxCount)
+    line(numNodes, algorithm,current,nodeNo+1, [current|list],maxCount,failureCount)
   end
 
-  def line(numNodes, algorithm, left, nodeNo, list,maxCount) when nodeNo>numNodes do
+  def line(numNodes, algorithm, left, nodeNo, list,maxCount,failureCount) when nodeNo>numNodes do
+    if(failureCount>0) do
+      killSomeRandomProcesses(failureCount,list)
+      IO.puts to_string(:os.system_time(:millisecond))<>"\t"<>to_string(failureCount)<>" nodes killed after creating topology"
+    end
     if String.equivalent?(algorithm,"GOSSIP") do
-      Project2.LineServer.sendGossip(Enum.random(list), "Abra ka dabra")
+      pehla=Enum.random(list)
+      IO.puts to_string(:os.system_time(:millisecond))<>"\tNode: "<>(List.to_string(:erlang.pid_to_list(pehla)) <> "\thas has started spreading the gossip")
+      Project2.LineServer.sendGossip(pehla, "Abra ka dabra")
       unlimitedLoop(list) 
     else
       if String.equivalent?(algorithm,"PUSH-SUM") do
-        Project2.LineServer.sendPushSum(Enum.random(list))  
+        pehla=Enum.random(list)
+        IO.puts to_string(:os.system_time(:millisecond))<>"\tNode: "<>(List.to_string(:erlang.pid_to_list(pehla)) <> "\thas started push sum")
+        Project2.LineServer.sendPushSum(pehla)  
         unlimitedLoop(list) 
       else
         IO.puts("INCORRECT ALGORITHM")
@@ -57,21 +65,29 @@ defmodule Project2 do
   end
   #FULL NETWORK #######################################################################################
   #to be called when FullNetwork topology is requested
-  def fullNetwork(numNodes, algorithm,list,nodeNo,maxCount) when nodeNo<=numNodes do
+  def fullNetwork(numNodes, algorithm,list,nodeNo,maxCount,failureCount) when nodeNo<=numNodes do
     {:ok, current}=Project2.FullNetworkServer.start_link(nodeNo,maxCount)
     Project2.FullNetworkServer.setCurrent(current)
-    fullNetwork(numNodes, algorithm,[current|list],nodeNo+1,maxCount)
+    fullNetwork(numNodes, algorithm,[current|list],nodeNo+1,maxCount,failureCount)
   end
-  def fullNetwork(numNodes, algorithm,list,nodeNo,maxCount) when nodeNo>numNodes do
+  def fullNetwork(numNodes, algorithm,list,nodeNo,maxCount,failureCount) when nodeNo>numNodes do
     for pid <- list do
       Project2.FullNetworkServer.setMyAddressBook(pid,list)
     end
+    if(failureCount>0) do
+      killSomeRandomProcesses(failureCount,list)
+      IO.puts to_string(:os.system_time(:millisecond))<>"\t"<>to_string(failureCount)<>" nodes killed after creating topology"
+    end
     if String.equivalent?(algorithm,"GOSSIP") do
-      Project2.FullNetworkServer.sendGossip(Enum.random(list), "Abra ka dabra")
+      pehla=Enum.random(list)
+      IO.puts to_string(:os.system_time(:millisecond))<>"\tNode: "<>(List.to_string(:erlang.pid_to_list(pehla)) <> "\thas has started spreading the gossip")
+      Project2.FullNetworkServer.sendGossip(pehla, "Abra ka dabra")
       unlimitedLoop(list) 
     else
       if String.equivalent?(algorithm,"PUSH-SUM") do
-        Project2.FullNetworkServer.sendPushSum(Enum.random(list))  
+        pehla=Enum.random(list)
+        IO.puts to_string(:os.system_time(:millisecond))<>"\tNode: "<>(List.to_string(:erlang.pid_to_list(pehla)) <> "\thas started push sum")
+        Project2.FullNetworkServer.sendPushSum(pehla)  
         unlimitedLoop(list) 
       else
         IO.puts("INCORRECT ALGORITHM")
@@ -81,7 +97,7 @@ defmodule Project2 do
 
   #IMPERFECT 2D GRID ###################################################################################
   #to be called when Imperfect2DGrid topology is requested
-  def imperfect2DGrid(topology,numNodes, algorithm,nodeNo,maxCount,x,y,map,nodeList) when nodeNo<=numNodes do
+  def imperfect2DGrid(topology,numNodes, algorithm,nodeNo,maxCount,x,y,map,nodeList,failureCount) when nodeNo<=numNodes do
     {:ok, current}=Project2.Imperfect2DGrid.start_link(nodeNo,maxCount,x,y)
     if rem(nodeNo,trunc(:math.sqrt(numNodes)))==0 do
       new_x=0
@@ -92,9 +108,9 @@ defmodule Project2 do
     end
     map=Map.put(map,{x,y},current)
     Project2.Imperfect2DGrid.setCurrent(current)
-    imperfect2DGrid(topology,numNodes, algorithm,nodeNo+1,maxCount,new_x,new_y,map,[current|nodeList])
+    imperfect2DGrid(topology,numNodes, algorithm,nodeNo+1,maxCount,new_x,new_y,map,[current|nodeList],failureCount)
   end
-  def imperfect2DGrid(topology,numNodes, algorithm,nodeNo,maxCount,x,y,map,nodeList) when nodeNo>numNodes do
+  def imperfect2DGrid(topology,numNodes, algorithm,nodeNo,maxCount,x,y,map,nodeList,failureCount) when nodeNo>numNodes do
     for pid <- nodeList do
       #Project2.Imperfect2DGrid.printNode(pid)
       xyMap=Project2.Imperfect2DGrid.getXY(pid)
@@ -111,12 +127,20 @@ defmodule Project2 do
       Project2.Imperfect2DGrid.setMyAddressBook(pid,addressBook)
       #Project2.Imperfect2DGrid.printNode(pid)
     end
+    if(failureCount>0) do
+      killSomeRandomProcesses(failureCount,nodeList)
+      IO.puts to_string(:os.system_time(:millisecond))<>"\t"<>to_string(failureCount)<>" nodes killed after creating topology"
+    end
     if String.equivalent?(algorithm,"GOSSIP") do
-      Project2.Imperfect2DGrid.sendGossip(Enum.random(nodeList), "Abra ka dabra")
+      pehla=Enum.random(nodeList)
+      IO.puts to_string(:os.system_time(:millisecond))<>"\tNode: "<>(List.to_string(:erlang.pid_to_list(pehla)) <> "\thas has started spreading the gossip")
+      Project2.Imperfect2DGrid.sendGossip(pehla, "Abra ka dabra")
       unlimitedLoop(nodeList) 
     else
       if String.equivalent?(algorithm,"PUSH-SUM") do
-        Project2.Imperfect2DGrid.sendPushSum(Enum.random(nodeList))  
+        pehla=Enum.random(nodeList)
+        IO.puts to_string(:os.system_time(:millisecond))<>"\tNode: "<>(List.to_string(:erlang.pid_to_list(pehla)) <> "\thas started push sum")
+        Project2.Imperfect2DGrid.sendPushSum(pehla)  
         unlimitedLoop(nodeList) 
       else
         IO.puts("INCORRECT ALGORITHM")
@@ -129,6 +153,16 @@ defmodule Project2 do
   def unlimitedLoop(list) do
     #IO.puts Process.whereis(Enum.random(list))
     unlimitedLoop([])
+  end
+
+  #Failsafing the code by killing nodes purposely
+  #this function kills a number of PIDs passed in the commandline arguements
+  def killSomeRandomProcesses(x, processList) do
+    deathRow=Enum.take_random(processList,x)
+    for pid <- deathRow do
+      IO.puts to_string(:os.system_time(:millisecond))<>"\tNode: "<>(List.to_string(:erlang.pid_to_list(pid)) <> "\thas been killed explicitly")
+      GenServer.stop(pid,:normal )
+    end
   end
 
 
